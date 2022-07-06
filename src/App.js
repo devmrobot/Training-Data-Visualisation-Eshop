f103ca657f468ce2775a1b936f1079719f2717b2
GUILLAUME PINOT <guillaume.pinott@gmail.com>
2022-07-06 17:29:19 +0200

    context navbar + sidebar done

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import "./app.css";
 import { Home } from "./pages/Home.jsx";
 import { SideBar } from "./component/sidebar/SideBar";
 
+
 function App() {
+
   return (
     <div className="App">
       <SideBar />
+      {/* <div className="">
+        {activeMenu ? (
+          <div className="nav-open">
+            <SideBar />
+          </div>
+        ) : (
+          <div className="nav-closed">
+            <SideBar />
+          </div>
+        )}
+      </div> */}
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/component/sidebar/SideBar.jsx
+++ src/component/sidebar/SideBar.jsx
@@ -1,43 +1,95 @@
 import React from "react";
-import "./sideBar.css"
+import "./sideBar.css";
 
-import { AiFillHome } from 'react-icons/ai';
-import { AiOutlineUser } from 'react-icons/ai';
-import { AiOutlineShopping } from 'react-icons/ai';
-import { BiMessageRoundedDots } from 'react-icons/bi';
+import { AiFillHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineShopping } from "react-icons/ai";
+import { BiMessageRoundedDots } from "react-icons/bi";
+
+import { useStateContext } from "../../contexts/ContextProvider";
 
 export const SideBar = () => {
+
+  const { activeMenu } = useStateContext();
+
   return (
     <>
       <div className="container">
-        <div className="navigation">
-          <ul>
-            <li>
-              <a href="#">
-                <span><AiFillHome className="icon"/></span>
-                <h1 className="main-title">Dashboard</h1>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span><AiOutlineUser className="icon"/></span>
-                <span className="title">Visitors</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span ><AiOutlineShopping className="icon"/></span>
-                <span className="title">Products</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span ><BiMessageRoundedDots className="icon"/></span>
-                <span className="title">Messages</span>
-              </a>
-            </li>
-          </ul>
-        </div>
+        {activeMenu ? (
+            <div className="navigation-open">
+              <ul>
+                <li>
+                  <a href="#">
+                    <span>
+                      <AiFillHome className="icon" />
+                    </span>
+                    <h1 className="main-title">Dashboard</h1>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span>
+                      <AiOutlineUser className="icon" />
+                    </span>
+                    <span className="title">Visitors</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span>
+                      <AiOutlineShopping className="icon" />
+                    </span>
+                    <span className="title">Products</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span>
+                      <BiMessageRoundedDots className="icon" />
+                    </span>
+                    <span className="title">Messages</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+        ) : (
+            <div className="navigation-closed">
+              <ul>
+                <li>
+                  <a href="#">
+                    <span>
+                      <AiFillHome className="icon" />
+                    </span>
+                    <h1 className="main-title">Dashboard</h1>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span>
+                      <AiOutlineUser className="icon" />
+                    </span>
+                    <span className="title">Visitors</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span>
+                      <AiOutlineShopping className="icon" />
+                    </span>
+                    <span className="title">Products</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span>
+                      <BiMessageRoundedDots className="icon" />
+                    </span>
+                    <span className="title">Messages</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+        )}
       </div>
     </>
   );
