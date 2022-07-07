804f09b6a42744a61d63127e967a7f030fa528f7
GUILLAUME PINOT <guillaume.pinott@gmail.com>
2022-07-07 17:54:20 +0200

    chart.js done

--- src/component/chart/BarChart.jsx
+++ src/component/chart/BarChart.jsx
@@ -1,8 +1,130 @@
-import React from 'react'
-import { Bar } from 'chart.js'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export const BarChart = () => {
+  const [chartData, setChartData] = useState({});
+
+  const Chart = () => {
+    let empBrand = [];
+    let empPrice = [];
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/products/brand-article-ordered`)
+      .then((res) => {
+        console.log(res);
+        for (const dataObj of res.data) {
+          empBrand.push(dataObj.brand);
+          empPrice.push(dataObj.price);
+        }
+        setChartData({
+          labels: empBrand,
+          datasets: [{
+              label: "level of thicceness",
+              data: empBrand,
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    Chart();
+  }, []);
+
+
   return (
-    <div>BarChart</div>
-  )
-}
+    <div className="App">
+      <h1>Bar Chart</h1>
+      <div>
+        <Bar
+          data={chartData}
+          options={{
+            responsive: true,
+            title: { text: "THICCNESS SCALE", display: true },
+            scales: {
+              yAxes: {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
+};
--- src/component/chart/PolarChart.jsx
+++ src/component/chart/PolarChart.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
-import { Polar } from 'chart.js'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { PolarArea } from "react-chartjs-2";
 
-export const PolarChart = () => {
+ChartJS.register(
+  RadialLinearScale,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const PolarChart = () => {
+
+  const [caBrands, setCaBrands] = useState([]);
+
+      //CA BY BRAND
+      useEffect(() => {
+        axios
+          .get(`${process.env.REACT_APP_API_URL}/products/ca-by-brand`)
+          .then((res) => res.data)
+          .then((data) => setCaBrands(data));
+      }, []);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Revenue by Brand",
+      },
+    },
+  };
+
+  const data = {
+    labels: caBrands.map((caBrand) => caBrand.brand),
+    datasets: [
+      {
+        label: "",
+        data: caBrands.map((caBrand) => caBrand.price),
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(75, 192, 192)',
+            'rgb(255, 205, 86)',
+        ]
+      },
+    ],
+  };
   return (
-    <div>PolarChart</div>
-  )
+    <>
+      <div>
+        <PolarArea options={options} data={data} />
+      </div>
+    </>
+  );
 }
+
+export default PolarChart;
