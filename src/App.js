b95bde402d47d84a7a8ede146bff21d3519c14b1
GUILLAUME PINOT <guillaume.pinott@gmail.com>
2022-07-08 14:44:03 +0200

    change app.cs to App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import "./app.css";
+import "./App.css";
 import { Home } from "./pages/Home.jsx";
 import { SideBar } from "./component/sidebar/SideBar";
 
